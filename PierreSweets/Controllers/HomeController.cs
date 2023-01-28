62bfeea53d284ab009e72e22498fb30b744b43ba
OLGON92 <ogon318@gmail.com>
2023-01-28 14:50:41 -0800

    add preliminary code to homeController

---
+++ PierreSweets/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using PierreSweets.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+
+namespace PierreSweets.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly PierreSweetsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public HomeController(UserManager<ApplicationUser> userManager, PierreSweetsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public async Task<ActionResult> Index()
+    {
+      Treat[] treats = _db.Treats.ToArray();
+      Flavor[] flavors = _db.Flavors.ToArray();
+      Dictionary<string,object[]> model = new Dictionary<string,object[]>();
+      model.Add("treats", treats);
+      model.Add("flavors", flavors);
+      return View(model);
+    }
+  }
+}
